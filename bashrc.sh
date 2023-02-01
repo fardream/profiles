e71ba80791af5ad85aeeb2217c2c24029b8bb8be
Chao Xu <fardream@users.noreply.github.com>
2023-02-01 14:15:14 +0000

    more setup

---
+++ bashrc.sh
@@ -0,0 +1,75 @@
+# .bashrc
+
+# User specific environment
+if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]
+then
+    PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+fi
+export PATH
+
+# Uncomment the following line if you don't like systemctl's auto-paging feature:
+# export SYSTEMD_PAGER=
+
+# User specific aliases and functions
+
+# Color mapping
+grey='\[\033[1;30m\]'
+red='\[\033[0;31m\]'
+RED='\[\033[1;31m\]'
+green='\[\033[0;32m\]'
+GREEN='\[\033[1;32m\]'
+yellow='\[\033[0;33m\]'
+YELLOW='\[\033[1;33m\]'
+purple='\[\033[0;35m\]'
+PURPLE='\[\033[1;35m\]'
+white='\[\033[0;37m\]'
+WHITE='\[\033[1;37m\]'
+blue='\[\033[0;34m\]'
+BLUE='\[\033[1;34m\]'
+cyan='\[\033[0;36m\]'
+CYAN='\[\033[1;36m\]'
+NC='\[\033[0m\]'
+
+export PS1="$yellow[$CYAN\u$yellow@$red\H$yellow $GREEN\W$grey$yellow]$NC\$ "
+
+export GPG_TTY=$(tty)
+
+source /opt/rh/gcc-toolset-12/enable
+
+alias e="emacs"
+export EDITOR=emacs
+
+alias bazel=bazelisk
+
+export HISTCONTROL=ignoredups:erasedups  # no duplicate entries
+export HISTSIZE=100000                   # big big history
+export HISTFILESIZE=100000               # big big history
+shopt -s histappend                      # append to history, don't overwrite it
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+# turn the below off.
+# After each command, append to the history file and reread it
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
+# End of saving histories from different terminals
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export PATH="${HOME}/mybin/bin":${PATH}
+
+export GOPATH="$HOME/go"; export GOROOT="$HOME/.go"; export PATH="$GOPATH/bin:$PATH"; # g-install: do NOT edit, see https://github.com/stefanmaric/g
+export GOBIN=${HOME}/gobin
+export PATH=${HOME}/gobin${PATH:+":${PATH}"}
+
+function man() {
+	emacs -nw --eval "(progn (man \"$1\") (delete-window))"
+}
